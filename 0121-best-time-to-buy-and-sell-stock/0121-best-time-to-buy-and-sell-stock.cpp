bc8b4e77c92d1e165a068dfdca63327a29828055
Anshika shukla <kanshikashukia235@gmail.com>
2026-08-19 15:58:59 +0530

    Time: 2 ms (30.95%), Space: 97.3 MB (61.05%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // int n = prices.size();
+
+        // int profit = 0;
+
+        
+
+        // for(int i=0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         if(prices[i] < prices[j]){
+        //             profit = max(profit,prices[j] - prices[i]);
+        //         }
+                
+        //     }
+        // }
+       
+        // return profit;
+
+        // optimal approach 
+
+        int n = prices.size();
+
+        int i = 0;
+        int j = 1;
+
+        int profit = 0;
+
+        while(j < n){
+            if(prices[j] < prices[i]){
+                i = j;
+            }
+            else{
+                profit = max(profit,prices[j] - prices[i]);
+            }
+            j++;
+        }
+        return profit;
+    }
+};
