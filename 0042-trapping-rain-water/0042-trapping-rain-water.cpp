2cf615980f024056cd091b7f3fcebc0081681ff4
Anshika shukla <kanshikashukia235@gmail.com>
2026-08-23 11:12:03 +0530

    Time: 0 ms (100%), Space: 27.1 MB (33.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -55,35 +55,57 @@ public:
 
         // return totalwater;
 
-        // Two pointer
+        // // Two pointer
 
-        int n = height.size();
-        int left = 0;
-        int right = n-1;
+        // int n = height.size();
+        // int left = 0;
+        // int right = n-1;
 
-        int leftmax = 0;
-        int rightmax = 0;
+        // int leftmax = 0;
+        // int rightmax = 0;
+        // int totalwater = 0;
+
+        // while(left < right){
+        //     if(height[left] < height[right]){
+        //         if(height[left] >= leftmax){
+        //             leftmax = height[left];
+        //         }
+        //         else{
+        //             totalwater += leftmax - height[left];
+        //         }
+        //         left++;
+        //     }
+        //     else{
+        //         if(height[right] >= rightmax){
+        //             rightmax = height[right];
+        //         }
+        //         else{
+        //             totalwater += rightmax - height[right];
+        //         }
+        //         right--;
+        //     }
+        // }
+        // return totalwater;
+
+        // Monotonic stack
+
+        stack<int>st;
         int totalwater = 0;
 
-        while(left < right){
-            if(height[left] < height[right]){
-                if(height[left] >= leftmax){
-                    leftmax = height[left];
+        for(int i=0; i<height.size(); i++){
+            while(!st.empty() && height[i] > height[st.top()]){
+                int bottom = st.top();
+                st.pop();
+                if(st.empty()){
+                    break;
                 }
-                else{
-                    totalwater += leftmax - height[left];
-                }
-                left++;
+                int left = st.top();
+                int boundedheight = min(height[left],height[i]) - height[bottom];
+
+                int width = i-left-1;
+                totalwater += boundedheight * width;
             }
-            else{
-                if(height[right] >= rightmax){
-                    rightmax = height[right];
-                }
-                else{
-                    totalwater += rightmax - height[right];
-                }
-                right--;
-            }
+            st.push(i);
         }
         return totalwater;
     }
